5f29cc56690be413a1e574c39e291de171d69e54
Andrés Estévez Costas <aecostas@gmail.com>
2020-06-11 19:41:31 +0200

    Refactor search

--- controllers/race.js
+++ controllers/race.js
@@ -1,40 +1,33 @@
 const list = (req, res) => {
     const races = [
         {
+            id:1,
             name:'Campeonato de Galicia',
             date: '01/08/2020',
             start: 'Gondomar'
         },
         {
+            id:2,
             name: 'Vuelta a España',
             date: '01/10/2020',
             start: 'Murcia'
         }
     ]
 
+    let result = [...races];
+
     const { name } = req.query;
     const { start } = req.query;
 
-    if ((!name) && (!start)){
-        res.json(races);
-        return;
-    } 
-    
-    if (!name && start) {
-        res.json(races.filter(race => race.start.toLowerCase().indexOf(start.toLowerCase()) !== -1) );
-        return;
-    } 
+    if (name) {
+        result = result.filter(race => race.name.toLowerCase().indexOf(name.toLowerCase()) !== -1) 
+    }
 
-    if (name && !start) {
-        res.json(races.filter(race => race.name.toLowerCase().indexOf(name.toLowerCase()) !== -1) );
-        return;
-    } 
+    if (start) {
+        result = result.filter(race => race.start.toLowerCase().indexOf(start.toLowerCase()) !== -1) 
+    }
 
-    res.json(races.filter(race => {
-        return ((race.name.toLowerCase().indexOf(name.toLowerCase()) !== -1) && 
-            ((race.start.toLowerCase().indexOf(start.toLowerCase()) !== -1)))
-    }))
-
+    res.json(result);
 }
 
 module.exports = {
